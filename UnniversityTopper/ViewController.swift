3f37b3d763b9c9b3d9c0fa43447f32f7520f7da5
Shamali Patil <shamalirpatil@gmail.com>
2020-08-13 21:15:35 +0100

    Initial Commit

---
+++ UnniversityTopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnniversityTopper
+//
+//  Created by Shamali Patil on 13/08/2020.
+//  Copyright © 2020 Shamali Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
